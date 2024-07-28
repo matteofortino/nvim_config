c8ad3efefbc5b6a67b1731079c5753bcbf430838
Matteo Fortino <matteofortino04@gmail.com>
2024-07-28 20:12:14 +0200

    change on_attact to a function to provide cpp code completition and errors handling

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -16,7 +16,10 @@ for _, lsp in ipairs(servers) do
 end
 
 lspconfig.clangd.setup {
-  on_attach = on_attach,
+  on_attach = function (client, bufnr)
+   client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
   on_init = on_init,
   capabilities = capabilities,
 }
