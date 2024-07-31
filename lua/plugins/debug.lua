db0cd8c354e2aa7693e274c09c05fbc40482223e
Matteo Fortino <matteofortino04@gmail.com>
2024-07-31 13:51:02 +0200

    add debugger for cpp files

---
+++ lua/plugins/debug.lua
@@ -0,0 +1,45 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+  },
+  config = function()
+    local dap = require "dap"
+    local dapui = require "dapui"
+
+    dapui.setup()
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+
+    -- cpp config
+    dap.adapters.gdb = {
+      type = "executable",
+      command = "/usr/local/bin/gdb",
+      args = { "-i", "dap" }
+    }
+    dap.configurations.cpp = {
+      {
+        name = "Launch",
+        type = "gdb",
+        request = "launch",
+        program = function()
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = "${workspaceFolder}",
+        stopAtBeginningOfMainSubprogram = false,
+      },
+    }
+  end,
+}
